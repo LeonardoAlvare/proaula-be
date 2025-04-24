d9cf4b4173802d80b32a49c86ab86e7bc6900d6e
Leonardo Alvarez <leonardoalvarezr12@gmail.com>
2025-04-24 15:39:03 -0500

    backend

---
+++ src/project/project.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { ProjectService } from './project.service';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
+
+@Controller('project')
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+
+  @Post()
+  create(@Body() createProjectDto: CreateProjectDto) {
+    return this.projectService.create(createProjectDto);
+  }
+
+  @Get()
+  findAll(@Query('nameFilter') name: string) {
+    return this.projectService.findAll(name);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.projectService.findOne(id);
+  }
+
+  @Get('user/:id')
+  findByUser(@Param('id') id: string) {
+    return this.projectService.findByUser(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
+    return this.projectService.update(id, updateProjectDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.projectService.remove(id);
+  }
+}
